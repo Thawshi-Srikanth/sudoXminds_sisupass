b81bcef6e73da2969417aab332c6de5ae80c0bc6
Thawshi Srikanth <100839102+Thawshi-Srikanth@users.noreply.github.com>
2025-08-13 02:18:57 +0000

    comp:added cal

---
+++ frontend/src/app/pass/add-new/page.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Home() {
+  return (
+    <>
+      <div className="col-span-6 flex justify-between w-full px-2">
+        <div className="flex justify-between flex-col">
+          <h1 className="scroll-m-20  text-2xl font-bold text-balance">
+            Your Passes
+          </h1>
+          <p className=" text-base tracking-wide">
+            Manage all your passes here.
+          </p>
+        </div>
+      </div>
+      <div className="col-span-6 flex justify-between w-full px-2">
+        <div className="grid grid-cols-4 w-full">
+          <div className="col-span-4 grid w-full  items-center gap-3">
+            <Label htmlFor="email">Name</Label>
+            <Input type="name" id="name" placeholder="Name" />
+          </div>
+
+          <div className="col-span-4 grid w-full  items-center gap-3">
+            <Label htmlFor="email">Exp</Label>
+ 
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
