3fbae4fde87be47456f8490d2b932ea27f1e3c2e
Thawshi Srikanth <100839102+Thawshi-Srikanth@users.noreply.github.com>
2025-08-15 01:43:52 +0000

    feat: set up Django backend with custom user model and authentication

---
+++ django-backend/authentication/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    def __str__(self):
+        return self.email
